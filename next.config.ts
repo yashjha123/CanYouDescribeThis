c2c33544b824e1de17e241a41ce8c2ff9f4367b9
Yash Jha <yjha1@ualberta.ca>
2025-05-08 16:03:27 +0530

    feat completely remove es lint checks in build stage

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://picsum.photos/**'), new URL("https://okwtyfgqkhtwiouamzid.supabase.co/**")],
   },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
